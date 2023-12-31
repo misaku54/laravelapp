7bff8ebdd30e6c73e06352c2765d388945f55a90
misaku54 <eisei0504@outlook.jp>
2023-12-31 17:30:56 +0900

    [update]タイポ修正

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,12 @@ $html = <<<EOF
 <title>Hello</title>
 <style>
 body { font-size:16pt; color:#999; }
-h1 { font-size:100pt; text-algin:right; color:#eee;
+h1 { font-size:100pt; text-align:right; color:#eee;
     margin:-40px 0px -50px 0px; }
 </style>
 </head>
 <body>
-    <h1>hello</h1>
+    <h1>Hello</h1>
     <p>This is sample page.</p>
     <p>これは、サンプルで作ったページです。</p>
 </body>
