0fe9efbc734f745c73cc08eb75013d3ce21a978c
misaku54 <eisei0504@outlook.jp>
2023-12-31 17:29:21 +0900

    [update]2-4

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', function () {
-    return '<html><body><h1>hello</h1><p>This is sample page.</p></body></html>';
+
+$html = <<<EOF
+<head>
+<title>Hello</title>
+<style>
+body { font-size:16pt; color:#999; }
+h1 { font-size:100pt; text-algin:right; color:#eee;
+    margin:-40px 0px -50px 0px; }
+</style>
+</head>
+<body>
+    <h1>hello</h1>
+    <p>This is sample page.</p>
+    <p>これは、サンプルで作ったページです。</p>
+</body>
+EOF;
+
+Route::get('hello', function () use ($html) {
+    return $html;
 });
