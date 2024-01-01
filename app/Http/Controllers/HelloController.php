acb1bf2895dd0913ecf558c7577af7204a9c1f61
misaku54 <eisei0504@outlook.jp>
2024-01-01 17:35:06 +0900

    [update]3-15

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,7 +10,16 @@ class HelloController extends Controller
     public function index() 
     {
         $data = [
-            'msg'=>'これはbladeを利用したサンプル',
+            'msg'=>'お名前を入力してください。',
+        ];
+        return view('hello.index', $data);
+    }
+    
+    public function post(Request $request)
+    {
+        $msg = $request->msg;
+        $data = [
+            'msg'=>'こんにちは、' . $msg . 'さん！',
         ];
         return view('hello.index', $data);
     }
