be46e7705ff65c2ac985f31bb2b5059e2821b65a
misaku54 <eisei0504@outlook.jp>
2024-01-04 04:27:50 +0900

    [fix]cssタイポ

--- resources/views/layouts/helloapp.blade.php
+++ resources/views/layouts/helloapp.blade.php
@@ -3,13 +3,13 @@
     <title>@yield('title')</title>
     <style>
     body { font-size: 16pt; color :#999; margin: 5px; }
-    h1 { font-size: 50pt; text-algin: right; color: #f6f6f6;
+    h1 { font-size: 50pt; text-align: right; color: #f6f6f6;
         margin: -20px 0px -30px 0px; letter-spacing: 4pt; }
-    ul { font-size: 12px; }
-    hr { margin: 25px 100pxl border-top: 1px dashed #ddd; }
+    ul { font-size: 12pt; }
+    hr { margin: 25px 100px; border-top: 1px dashed #ddd; }
     .menutitle { font-size: 14px; font-weight: bold; margin: 0px; }
     .content { margin: 10px; }
-    .footer { text-algin: right; font-size: 10pt; margin: 10px;
+    .footer { text-align: right; font-size: 10pt; margin: 10px;
         border-bottom: solid 1px #ccc; color: #ccc; }
     </style>
 </head>
