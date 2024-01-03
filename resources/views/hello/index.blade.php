5e5991132c3058467a755382b34bed796c416770
misaku54 <eisei0504@outlook.jp>
2024-01-04 05:27:26 +0900

    [update]3-32

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,10 +9,9 @@
 
 @section('content')
     <p>ここが本文のコンテンツです。</p>
-    <p>必要なだけ記述できます。</p>
-    
-    @include('components.message',['msg_title'=>'ok',
-        'msg_content'=>'サブビューです。'])
+    <ul>
+    @each('components.item', $data, 'item')
+    </ul>
 @endsection
 
 @section('footer')
